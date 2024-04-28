d69a958e18d2f4f03932f0667239ea1a7dacff2e
smy1999 <865988027@qq.com>
2024-04-28 23:35:49 +0800

    240428 Update activity stateflow base test.

--- lottery-interfaces/src/test/java/com/moon/lottery/BaseTest.java
+++ lottery-interfaces/src/test/java/com/moon/lottery/BaseTest.java
@@ -1,6 +1,5 @@
 package com.moon.lottery;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author: smy1999
  * @date: 2024/4/17
  */
-@Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class BaseTest {
--- lottery-interfaces/src/test/java/com/moon/lottery/domain/activity/stateflow/ArraignmentStateTest.java
+++ lottery-interfaces/src/test/java/com/moon/lottery/domain/activity/stateflow/ArraignmentStateTest.java
@@ -2,7 +2,10 @@ package com.moon.lottery.domain.activity.stateflow;
 
 import com.moon.lottery.BaseTest;
 import com.moon.lottery.common.Constants;
+import com.moon.lottery.common.Result;
 import com.moon.lottery.domain.activity.service.stateflow.ArraignmentState;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
 import javax.annotation.Resource;
 
@@ -11,13 +14,16 @@ import javax.annotation.Resource;
  * @author: smy1999
  * @date: 2024/4/28
  */
+@Slf4j
 public class ArraignmentStateTest extends BaseTest {
 
     @Resource
     private ArraignmentState arraignmentState;
 
+    @Test
     public void testPass() {
-        arraignmentState.pass(888L, Constants.ActivityState.PASS);
+        Result result = arraignmentState.pass(888L, Constants.ActivityState.ARRAIGNMENT);
+        log.info("{}", result.getInfo());
     }
 
 }
